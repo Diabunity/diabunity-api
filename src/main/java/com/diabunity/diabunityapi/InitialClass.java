1bc6a04fdcf3f3561f6aeb59151924a12d2d3c75
Marcelo Torres <marcelo.torres@mercadolibre.com>
2022-05-26 19:55:57 -0300

    added initial class

---
+++ src/main/java/com/diabunity/diabunityapi/InitialClass.java
@@ -0,0 +1,13 @@
+package com.diabunity.diabunityapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitialClass {
+
+  public static void main(String[] args) {
+    SpringApplication.run(InitialClass.class, args);
+  }
+
+}
