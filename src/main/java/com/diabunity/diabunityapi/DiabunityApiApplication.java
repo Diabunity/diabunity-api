f24afba0ba626b6eab9ea7e87a8ed0c5fdfa3741
Leonel San Martin <31077603+leo9210@users.noreply.github.com>
2022-05-31 14:14:46 -0300

    link api con Mongo DB Atlas

--- src/main/java/com/diabunity/diabunityapi/DiabunityApiApplication.java
+++ src/main/java/com/diabunity/diabunityapi/DiabunityApiApplication.java
@@ -3,11 +3,13 @@ package com.diabunity.diabunityapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class DiabunityApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiabunityApiApplication.class, args);
 	}
+
 
 }
