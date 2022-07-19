aa5426895bc5c0e5d0c37f72479d7546bf7a9cf2
Marcelo Torres <marcelo.torres@mercadolibre.com>
2022-07-19 20:04:04 -0300

    changed error messages

--- src/main/java/com/diabunity/diabunityapi/controllers/UserController.java
+++ src/main/java/com/diabunity/diabunityapi/controllers/UserController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,62 +22,67 @@ public class UserController {
   private UserService userService;
 
   @PostMapping("/users")
-  public ResponseEntity<User> createUser(@RequestBody User userData) {
-    if(userData.getId().isEmpty()) {
-      return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+  public ResponseEntity<User> createUser(@RequestBody User userData) throws Exception {
+    if(userData.getId() == null || userData.getId().isEmpty()) {
+      throw new Exception("User ID is required.");
     }
 
     User user = userService.saveUser(userData);
 
     if (user == null) {
-      return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+      throw new Exception("Save user failed.");
     }
 
     return new ResponseEntity<>(user, HttpStatus.CREATED);
   }
 
   @PutMapping("/users")
-  public ResponseEntity<User> updateUser(@RequestBody User userData) {
-    if(userData.getId().isEmpty()) {
-      return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+  public ResponseEntity<User> updateUser(@RequestBody User userData) throws Exception {
+    if(userData == null || userData.getId().isEmpty()) {
+      throw new Exception("User ID is required.");
     }
 
     Optional<User> user = userService.getUser(userData.getId());
 
     if (user == null) {
-      return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+      throw new Exception("User was not founded with used_id " + userData.getId());
     }
 
     if (userService.saveUser(userData) == null) {
-      return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+      throw new Exception("Save user failed.");
     }
 
     return new ResponseEntity<>(user.get(), HttpStatus.CREATED);
   }
 
   @GetMapping("/users/{id}")
-  public ResponseEntity<User> getUser(@PathVariable String id) {
+  public ResponseEntity<User> getUser(@PathVariable String id) throws Exception {
     if(id.isEmpty()) {
-      return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+      throw new Exception("User ID is required.");
     }
 
     Optional<User> user = userService.getUser(id);
 
     if (user == null) {
-      return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+      throw new Exception("User was not founded by user_id " + id);
     }
 
     return new ResponseEntity<>(user.get(), HttpStatus.CREATED);
   }
 
   @GetMapping("/users/")
-  public ResponseEntity<List<User>> getAllUser() {
+  public ResponseEntity<List<User>> getAllUser() throws Exception {
     List<User> users = userService.getAllUsers();
 
-    if (users == null) {
-      return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    if (users == null || users.isEmpty()) {
+      throw new Exception("Users were not founded");
     }
 
     return new ResponseEntity<>(users, HttpStatus.OK);
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity handleException(Exception e) {
+    return new ResponseEntity<>(e.getMessage() != "" ? e.getMessage() : e.getCause(), HttpStatus.BAD_REQUEST);
   }
 }
