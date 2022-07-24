5f8e4c06ef2766718a328f8f4c45abc4cc2ef3ae
Marcelo Torres <marcelo.torres@mercadolibre.com>
2022-07-24 12:24:46 -0300

    removed unused method

--- src/main/java/com/diabunity/diabunityapi/services/UserService.java
+++ src/main/java/com/diabunity/diabunityapi/services/UserService.java
@@ -2,7 +2,6 @@ package com.diabunity.diabunityapi.services;
 
 import com.diabunity.diabunityapi.models.User;
 import com.diabunity.diabunityapi.repositories.UserRepository;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,10 +18,6 @@ public class UserService {
 
   public Optional<User> getUser(final String id) {
    return userRepository.findById(id);
-  }
-
-  public List<User> getAllUsers() {
-    return userRepository.findAll();
   }
 
 }
