3a4920399ba0c9cb06fcc95405f3d54b2cfa6254
Marcelo Torres <marcelo.torres@mercadolibre.com>
2022-07-27 18:48:23 -0300

    changed path json file

--- src/main/java/com/diabunity/diabunityapi/auth/FirebaseAuthService.java
+++ src/main/java/com/diabunity/diabunityapi/auth/FirebaseAuthService.java
@@ -5,16 +5,11 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Objects;
 
 @Configuration
 public class FirebaseAuthService {
@@ -22,14 +17,8 @@ public class FirebaseAuthService {
     @Bean
     public FirebaseApp getFirebaseApp() throws IOException {
         ClassLoader classloader = DiabunityApiApplication.class.getClassLoader();
-        InputStream inputStream = classloader.getResourceAsStream("diabunity-dev-firebase-adminsdk-bg41i-bf2a2eb992.json");
-
-        File file = new File(Objects.requireNonNull(new InputStreamReader(inputStream));
-
-        FileInputStream serviceAccount = new FileInputStream(file.getAbsolutePath());
-
         FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setCredentials(GoogleCredentials.fromStream(classloader.getResourceAsStream("diabunity-dev-firebase-adminsdk-bg41i-bf2a2eb992.json")))
                 .build();
 
         FirebaseApp.initializeApp(options);
