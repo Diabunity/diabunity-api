03a62b57c25695a5d35683fa4c39f328689decf9
Marcelo Torres <marcelo.torres@mercadolibre.com>
2022-07-27 19:43:33 -0300

    changed swagger location

--- src/main/java/com/diabunity/diabunityapi/controllers/Swagger.java
+++ src/main/java/com/diabunity/diabunityapi/controllers/Swagger.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class Swagger {
     @RequestMapping("/swagger")
     public String index(Model modelo) {
-      return "index";
+      return "swagger";
     }
 
 }
