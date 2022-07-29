7775b69f27512d3bbd0582adac7735638d1f6ec7
Marcelo Torres <marcelo.torres@mercadolibre.com>
2022-07-29 15:52:48 -0300

    added measurements endpoint

--- src/main/java/com/diabunity/diabunityapi/controllers/MeasurementController.java
+++ src/main/java/com/diabunity/diabunityapi/controllers/MeasurementController.java
@@ -1,12 +1,17 @@
 package com.diabunity.diabunityapi.controllers;
 
+import com.diabunity.diabunityapi.exceptions.NotFoundException;
 import com.diabunity.diabunityapi.models.Measurement;
 import com.diabunity.diabunityapi.exceptions.InvalidUserTokenException;
 import com.diabunity.diabunityapi.services.MeasurementService;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -34,4 +39,29 @@ public class MeasurementController {
 
     return new ResponseEntity<>(res, HttpStatus.CREATED);
   }
+
+  @GetMapping("/users/{id}/measurements")
+  public ResponseEntity getAllMeasurementsByUserId(HttpServletRequest request,
+                                                   @PathVariable(value="id") String uid,
+                                                   @RequestParam("from") @DateTimeFormat(pattern="yyyyMMdd") Date from,
+                                                   @RequestParam("to")  @DateTimeFormat(pattern="yyyyMMdd") Date to) throws Exception {
+
+    String authorizedUser = request.getSession().getAttribute("authorized_user").toString();
+    if (!authorizedUser.equals(uid)) {
+      throw new InvalidUserTokenException();
+    }
+
+    List<Measurement> measurements = measurementService.getAllByUserId(uid,
+        LocalDateTime.ofInstant(from.toInstant(),
+            ZoneId.systemDefault()),
+        LocalDateTime.ofInstant(to.toInstant(),
+            ZoneId.systemDefault()));
+
+    if (measurements == null || measurements.isEmpty()) {
+      throw new NotFoundException("Measurements not found with user id: " + uid);
+    }
+
+    return new ResponseEntity<>(measurements, HttpStatus.OK);
+  }
+
 }
--- src/main/java/com/diabunity/diabunityapi/repositories/MeasurementRepository.java
+++ src/main/java/com/diabunity/diabunityapi/repositories/MeasurementRepository.java
@@ -1,8 +1,12 @@
 package com.diabunity.diabunityapi.repositories;
 
 import com.diabunity.diabunityapi.models.Measurement;
+import java.time.LocalDateTime;
+import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MeasurementRepository extends MongoRepository<Measurement, String> { }
+public interface MeasurementRepository extends MongoRepository<Measurement, String> {
+  List<Measurement> findAllByUserIdAndTimestampBetween(String id, LocalDateTime startDate, LocalDateTime endDate);
+}
