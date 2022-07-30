9092f4876b4d25ae5f43bae2df28f601475280ce
Marcelo Torres <marcelo.torres@mercadolibre.com>
2022-07-30 16:29:23 -0300

    Get measurements by user (#5)
    * changed path json file
    
    * changed path json file
    
    * changed path json file
    
    * changed path json file
    
    * changed swagger location
    
    * changed swagger location
    
    * added measurements endpoint
    
    * fixed messasge error status 400

--- src/main/java/com/diabunity/diabunityapi/exceptions/ExceptionHandlers.java
+++ src/main/java/com/diabunity/diabunityapi/exceptions/ExceptionHandlers.java
@@ -1,9 +1,12 @@
 package com.diabunity.diabunityapi.exceptions;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
@@ -30,6 +33,15 @@ public class ExceptionHandlers extends ResponseEntityExceptionHandler {
     @ExceptionHandler ( {BadRequestException.class} )
     public ResponseEntity handleBadRequestException(BadRequestException ex) {
         ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, ex.getMessage(), ex.getCauses());
+        return new ResponseEntity<>(apiError, apiError.getCode());
+    }
+
+    @Override
+    public ResponseEntity handleMissingServletRequestParameter(MissingServletRequestParameterException ex,
+                                                               HttpHeaders headers,
+                                                               HttpStatus status,
+                                                               WebRequest request) {
+        ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, ex.getMessage());
         return new ResponseEntity<>(apiError, apiError.getCode());
     }
 
