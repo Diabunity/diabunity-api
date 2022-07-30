ad3ef3e4b7204b5cbb669b444d8816e5a10bf851
Marcelo Torres <marcelo.torres@mercadolibre.com>
2022-07-29 23:45:53 -0300

    fixed messasge error status 400

--- src/main/java/com/diabunity/diabunityapi/controllers/MeasurementController.java
+++ src/main/java/com/diabunity/diabunityapi/controllers/MeasurementController.java
@@ -1,22 +1,26 @@
 package com.diabunity.diabunityapi.controllers;
 
+import static java.text.MessageFormat.format;
+
+import com.diabunity.diabunityapi.exceptions.InvalidUserTokenException;
 import com.diabunity.diabunityapi.exceptions.NotFoundException;
 import com.diabunity.diabunityapi.models.Measurement;
-import com.diabunity.diabunityapi.exceptions.InvalidUserTokenException;
 import com.diabunity.diabunityapi.services.MeasurementService;
-
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
 import java.util.List;
-
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MeasurementController {
@@ -58,7 +62,7 @@ public class MeasurementController {
             ZoneId.systemDefault()));
 
     if (measurements == null || measurements.isEmpty()) {
-      throw new NotFoundException("Measurements not found with user id: " + uid);
+      throw new NotFoundException(format("Measurements not found with user id: {0} and date between {1} and {2}", uid, from, to));
     }
 
     return new ResponseEntity<>(measurements, HttpStatus.OK);
