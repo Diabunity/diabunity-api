9f35643f74f7c0f6d43fbdfa9488729cbb89cb9f
Marcelo Torres <marcelo.torres@mercadolibre.com>
2022-08-23 23:57:34 -0300

    added get measurements wrapper

--- src/main/java/com/diabunity/diabunityapi/controllers/MeasurementController.java
+++ src/main/java/com/diabunity/diabunityapi/controllers/MeasurementController.java
@@ -4,6 +4,7 @@ package com.diabunity.diabunityapi.controllers;
 import com.diabunity.diabunityapi.exceptions.InvalidUserTokenException;
 import com.diabunity.diabunityapi.exceptions.NotFoundException;
 import com.diabunity.diabunityapi.models.Measurement;
+import com.diabunity.diabunityapi.models.MeasurementWrapper;
 import com.diabunity.diabunityapi.models.MeasurementsResponse;
 import com.diabunity.diabunityapi.models.User;
 import com.diabunity.diabunityapi.services.MeasurementService;
@@ -37,15 +38,15 @@ public class MeasurementController {
   @PostMapping("/users/{id}/measurements")
   public Object createMeasurements(HttpServletRequest request,
                                    @PathVariable(value="id") String uid,
-                                   @RequestBody List<Measurement> measurements) throws Exception {
+                                   @RequestBody MeasurementWrapper measurements) throws Exception {
 
     String authorizedUser = request.getSession().getAttribute("authorized_user").toString();
     if (!authorizedUser.equals(uid)) {
       throw new InvalidUserTokenException();
     }
 
-    measurements.forEach(m -> m.setUserId(uid));
-    List<Measurement> res = measurementService.saveAll(measurements);
+    measurements.getMeasurements().forEach(m -> m.setUserId(uid));
+    List<Measurement> res = measurementService.saveAll(measurements.getMeasurements());
 
     return new ResponseEntity<>(res, HttpStatus.CREATED);
   }
