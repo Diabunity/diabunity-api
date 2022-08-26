47951c3479e9b1bdfc41735cd71ac5bbda319df1
Ale Imperatori <alejandroimperatori@gmail.com>
2022-08-26 11:34:54 -0300

    fix enums json mapping

--- src/main/java/com/diabunity/diabunityapi/models/DiabetesType.java
+++ src/main/java/com/diabunity/diabunityapi/models/DiabetesType.java
@@ -1,6 +1,13 @@
 package com.diabunity.diabunityapi.models;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum DiabetesType {
   TYPE1,
-  TYPE2
+  TYPE2;
+
+  @JsonValue
+  public int toValue() {
+    return ordinal();
+  }
 }
--- src/main/java/com/diabunity/diabunityapi/models/MeasurementStatus.java
+++ src/main/java/com/diabunity/diabunityapi/models/MeasurementStatus.java
@@ -1,8 +1,14 @@
 package com.diabunity.diabunityapi.models;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum MeasurementStatus {
   LOW,
   OK,
   HIGH,
-  SUPER_HIGH
+  SUPER_HIGH;
+  @JsonValue
+  public int toValue() {
+    return ordinal();
+  }
 }
