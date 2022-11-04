e52f1d92ad078d9d290a97ac59474bdee8f7fafa
marceloatorres <marcelotorres.unlam@gmail.com>
2022-11-04 12:22:47 -0300

    changed path param on delete emoji

--- src/main/java/com/diabunity/diabunityapi/repositories/ReactionRepository.java
+++ src/main/java/com/diabunity/diabunityapi/repositories/ReactionRepository.java
@@ -1,16 +1,14 @@
 package com.diabunity.diabunityapi.repositories;
 
 import com.diabunity.diabunityapi.models.Reaction;
-import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ReactionRepository extends MongoRepository<Reaction, String> {
 
     List<Reaction> findByPostId(String postId);
 
-    void deleteByUserIdAndPostIdAndEmoji(String userId, String postId, String emoji);
+    void deleteByUserIdAndPostIdAndName(String userId, String postId, String name);
 
 }
