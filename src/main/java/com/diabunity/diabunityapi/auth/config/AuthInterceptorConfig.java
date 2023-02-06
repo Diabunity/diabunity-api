5304f6861c7f36994c3ed46e7ea9b47b835d6545
Ale Imperatori <alejandroimperatori@gmail.com>
2023-02-06 15:27:35 -0300

    Excludes / from auth interceptor

--- src/main/java/com/diabunity/diabunityapi/auth/config/AuthInterceptorConfig.java
+++ src/main/java/com/diabunity/diabunityapi/auth/config/AuthInterceptorConfig.java
@@ -14,6 +14,6 @@ public class AuthInterceptorConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
-        registry.addInterceptor(authenticationInterceptor).addPathPatterns("/**");
+        registry.addInterceptor(authenticationInterceptor).addPathPatterns("/**").excludePathPatterns("/");
     }
 }
