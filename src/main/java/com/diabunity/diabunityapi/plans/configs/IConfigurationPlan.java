da1f84be9d01f870381cffcc700d86d0171bd19f
Marcelo Torres <marcelo.torres@mercadolibre.com>
2023-02-21 22:36:24 -0300

    fixes

--- src/main/java/com/diabunity/diabunityapi/plans/configs/IConfigurationPlan.java
+++ src/main/java/com/diabunity/diabunityapi/plans/configs/IConfigurationPlan.java
@@ -4,7 +4,6 @@ import com.diabunity.diabunityapi.models.SubscriptionConfigs;
 import java.util.List;
 
 public interface IConfigurationPlan {
-    //HashMap<String, Integer> getConfigsAccordingPlan();
     List<SubscriptionConfigs> getConfigAccordingPlan();
     int getMaxPostsPerDay();
 }
